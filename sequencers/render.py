da26991794214124a5725821766e1f1de9e5810f
Martin Doudoroff <martin@doudoroff.com>
2021-02-15 14:13:40 -0500

    update

--- sequencers/render.py
+++ sequencers/render.py
@@ -98,13 +98,14 @@ var vtnrkjn = ['f','i','o','f','a','=','a','<','a','>','n','l','f','a','<','r','
 
 <h4>Latest</h4>
 <p class="updates">
+2021-02-15 added Intellijel Metropolix<br />
 2021-01-23 minor updates<br />
 2021-01-03 added Intellijel Tete+Tetrapad<br />
 2020-12-19 added D&D Modules Heaven 16<br />
 2020-12-12 added Extralife Super Sixteen, other updates<br />
 2020-10-25 added Ladik S-280<br />
-2020-08-01 added SDS Digital Sequarallel<br />
 <span id="additionalUpdates" class="collapsed">
+2020-08-01 added SDS Digital Sequarallel<br />
 2020-07-21 added (with misgivings) Behringer 960 and 182<br />
 2020-04-29 added the RYK M185<br />
 2020-03-30 reworked the Gate Handling column<br />
