691d12e4b113f52b23c009e10ebe55fc676ef111
Martin Doudoroff <martin@doudoroff.com>
2022-01-20 13:02:07 -0500

    update

--- quantizers/render.py
+++ quantizers/render.py
@@ -129,6 +129,7 @@ var vtnrkjn = ['f','i','o','f','a','=','a','<','a','>','n','l','f','a','<','r','
 
 <h4>Latest</h4>
 <p class="updates">
+2022-01-20 added Flame Tame Machine (historical)<br />
 2022-01-19 added Bastl 1983<br />
 2020-10-31 added Monome Crow and Der Mann mit der Maschine DROID<br />
 2020-02-19 added Shakmat Bard Quartet<br />
