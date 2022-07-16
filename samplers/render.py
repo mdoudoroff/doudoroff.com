ec11448cd4481188569ea3ca357a7a507f72c438
Martin Doudoroff <martin@doudoroff.com>
2022-07-16 14:30:45 -0400

    update

--- samplers/render.py
+++ samplers/render.py
@@ -127,6 +127,7 @@ var vtnrkjn = ['f','i','o','f','a','=','a','<','a','>','n','l','f','a','<','r','
 
 <h4>Latest</h4>
 <p class="updates">
+2022-07-16 added Mutable Instruments Beads<br />
 2022-05-31 minor updates<br />
 2022-05-29 added ADDAC112 and Miso Cornflakes<br />
 2021-07-28 added Endorphin.es Two of Cups<br />
